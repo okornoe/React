eb7290ab72afc86add2a2adba5ec39d7c2a262be
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-09-20 10:25:25 -0700

    Initial commit

---
+++ projects/tic-tac-toe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+
+      <h1>Software Engineering</h1>
+      <h4>Software Engineering Cards</h4>
+    </div>
+  )
+}
+
+export default App
