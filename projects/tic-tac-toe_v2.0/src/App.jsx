54475b1328ecea11111c4e0392b9934f4705ef41
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-10-17 15:04:25 +0000

    add board component

--- projects/tic-tac-toe_v2.0/src/App.jsx
+++ projects/tic-tac-toe_v2.0/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Square from './Square'
+import Square from './assets/components/Square'
 
 function App() {
   const [count, setCount] = useState(0)
