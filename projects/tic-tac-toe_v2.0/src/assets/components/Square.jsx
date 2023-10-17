ee404e788874af155cb29cbb67eb3dc125ca020c
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-10-17 15:18:30 +0000

    update square

--- projects/tic-tac-toe_v2.0/src/assets/components/Square.jsx
+++ projects/tic-tac-toe_v2.0/src/assets/components/Square.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Square() {
   return (
     <>
-        <div className='row-board'>
-          <button className='square'>1</button>
-        </div>
+      <button className='square'>1</button>
     </>
     
   )
